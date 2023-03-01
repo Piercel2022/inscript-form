93c06ea81471f6086642308cf2f27958ab6a6f8d
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-03-01 09:30:45 +0100

    Modify the workflow

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import myImage from './images/image01.png';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <img className="photo"  src={myImage} alt="" />
+    <div className="flexContainer">
+    <section> <h1>Loveworld Strasbourg</h1> 
+    <div>vous invite à découvrir ses différents clubs de lecture</div></section>
+    <div>Club adulte, les mardis 16h - 17h30 et les jeudis 16h30 - 18h00</div>
+    <div>Teevo club pour Ados, les samedis 15h30 - 17h00</div>
+    <div>Club Jeunes lecteurs, les mercredis 15h00 - 17h00</div>
+    <div>Participation volontaire de 10 EUR / mois</div>
+    <div>Lieu des clubs: Local Loveworld Strasbourg, rue des acacias, 67370 Pfulgriesheim</div>
+    <div>Inscription surplace</div>
+    </div>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
