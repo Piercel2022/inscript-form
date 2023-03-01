afd6f7d83b137d3f8f0100a7bcefd7073fe2f4e0
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-03-01 09:28:14 +0100

    Modify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import ModalDialog from './ModalDialog';
 
-function App() {
+const App = () => {
+  // declare a new state variable for modal open
+  const [open, setOpen] = useState(false);
+
+  // function to handle modal open
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  // function to handle modal close
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="contained" className="formulaire" color="primary" onClick={handleOpen}>
+        Formulaire
+      </Button>
+      <ModalDialog open={open} handleClose={handleClose} />
     </div>
   );
-}
+};
 
 export default App;
